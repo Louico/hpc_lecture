ce41b59db8cffa7fd3dadf1911e849079f556fef
louico <450125293@qq.com>
2020-05-12 19:41:20 +0900

    assignment_1

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -7,7 +7,8 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
   std::vector<T> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+
+  for (int i=0; i<tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -17,6 +18,7 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
     else
       tmp[i] = vec[right++]; 
   }
+
   for (int i=0; i<tmp.size(); i++) 
     vec[begin++] = tmp[i];
 }
@@ -25,8 +27,11 @@ template<class T>
 void merge_sort(std::vector<T>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+#pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
@@ -34,13 +39,18 @@ void merge_sort(std::vector<T>& vec, int begin, int end) {
 int main() {
   int n = 20;
   std::vector<int> vec(n);
+
   for (int i=0; i<n; i++) {
     vec[i] = rand() % (10 * n);
     printf("%d ",vec[i]);
   }
   printf("\n");
+#pragma omp parallel
+  {
+#pragma omp single
+    merge_sort(vec, 0, n-1);
+};
 
-  merge_sort(vec, 0, n-1);
 
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -12,19 +12,54 @@ int main() {
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
+  std::vector<int> bucket(range),index(range);
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
+#pragma omp parallel for shared(bucket)
   for (int i=0; i<n; i++) {
+#pragma omp atomic update
     bucket[key[i]]++;
   }
+/*
   for (int i=0, j=0; i<range; i++) {
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
+*/
 
+#pragma omp parallel for
+for (int i=0; i<range; i++){
+    index[i] = 0;
+#pragma omp parallel for
+    for (int j=0; j<i; j++){
+#pragma omp atomic update
+        index[i] += bucket[j];
+    }
+#pragma omp parallel for
+    for(int k = index[i]; k<index[i]+bucket[i]; k++){
+        key[k] = i;
+    }
+
+}
+
+/*
+#pragma omp parallel for
+    for (int i=0; i<range; i++){
+        int index = 0;
+#pragma omp parallel for reduction(+:index)
+        for (int j=0; j<i; j++){
+            index += bucket[j];
+        }
+#pragma omp parallel for
+        for(int k = bucket[i]; k>0; k--){
+            key[index++] = i;
+        }
+
+    }
+*/
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
